3d2243a0eb1f02008fa3886b8966f992dd208942
khalyel05311997 <41632457+khalyel05311997@users.noreply.github.com>
2018-07-27 16:49:26 +0200

    Add files via upload

---
+++ khalil7/js/main.js
@@ -0,0 +1,13 @@
+function sub(){
+
+
+if(f.n.value=="" && f.s.value=="" && f.t.value=="") 
+{
+ alert("erreurrrrrrrrrrrrrrrrrrr");
+
+}
+else{
+document.getElementById("thx").style.display="block";
+
+}
+}
